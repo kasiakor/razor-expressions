01fb34826935af132977d25f062fa903e3704450
kasia.kordys <kasia.kordys@gmail.com>
2020-07-31 19:02:40 +0100

    add basic model, view and controller

---
+++ RazorExpressions/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using RazorExpressions.Models;
+using System.Web.Mvc;
+
+namespace RazorExpressions.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for 2 people",
+            Category = "Lake",
+            Price = 22.00M
+        };
+        // GET: Home
+
+            //Index method creates and populates the properties of product object
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+    }
+}
